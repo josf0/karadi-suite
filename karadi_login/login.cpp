a2a520ca7af4565dd55da54eea52153c3d798b29
josf0 <josephmukkollil000@gmail.com>
2025-05-24 11:29:40 +0530

    Completed login interface

--- karadi_login/login.cpp
+++ karadi_login/login.cpp
@@ -6,26 +6,85 @@ using namespace std;
 #include <string>
 #include <fstream>
 
-class user {
-    public:
-        string username;
-        string password;
-}
-
 void register_user() {
+
+    string user_name;
+    string user_password;
+
     cout << "\nRegistering User\n";
+    ofstream UserDatabase("UserDatabase.txt");
+
+    cout << "\nEnter your username: ";
+    cin >> user_name;
+    UserDatabase << user_name << "\n";
+
+    cout << "\nEnter your password: ";
+    cin >> user_password;
+    UserDatabase << user_password << "\n";
+
+    cout << "\nUser has been regsitered.\n";
+
+    UserDatabase.close();
+
 }
 
 void login_user() {
-    cout << "\nLogging in User\n";
+
+    string user_name;
+    string user_password;
+    string db_name;
+    string db_password;
+    int count;
+
+    // Reading from UserDatabase file
+    ifstream UserDatabase("UserDatabase.txt");
+
+    getline(UserDatabase, db_name);
+    getline(UserDatabase, db_password);
+
+    UserDatabase.close();
+
+    cout << "\nWelcome to login screen\n";
+    cout << "\nEnter your username: ";
+    for(int i = 1; i < 4; i++) {
+        cin >> user_name;
+        if(user_name == db_name) {
+            cout << "\nCorrect username entered";
+            count++;
+            break;
+        }
+        else {
+            cout << "\nThe entered username doesn't exist";
+            cout << "\nTries left = " << i << "\nTry again: ";
+        }
+    }
+    cout << "\nEnter your password: ";
+    for(int i = 1; i < 4; i++) {
+        cin >> user_password;
+        if(user_password == db_password) {
+            cout << "\nCorrect password entered";
+            count++;
+            break;
+        }
+        else {
+            cout << "\nThe entered password doesn't exist";
+            cout << "\nTries left = " << i << "\nTry again: ";
+        }
+    }
+
+    if(count == 2) {
+        cout << "\n\n###################\nWelcome to Karadi-Suite\n###################\n";
+    }
+
 }
 
 int main() {
 
     int user_start;
     int user_regORlog;
+    string user_in;
 
-    cout << "\nWelcome to the Karadi-Suite\nWould you like to login or quit the app?\n1: Login\n2: Quit\nEnter your option: ";
+    cout << "\nWelcome to the Karadi-Suite\nWould you like to start or quit the app?\n1: Start\n2: Quit\nEnter your option: ";
     cin >> user_start;
 
     if(user_start == 1) {
@@ -34,6 +93,11 @@ int main() {
 
         if(user_regORlog == 1) {
             register_user();
+            cout << "\nWould you like to login?\nyes/no?: ";
+            cin >> user_in;
+            if(user_in == "yes" || user_in == "Yes") {
+                login_user();
+            }
         }
         else if(user_regORlog == 2) {
             login_user();
@@ -45,7 +109,7 @@ int main() {
     else {
         cout << "\nPlease enter a valid option";
     }
-    
+
 
     return 0;
 }
